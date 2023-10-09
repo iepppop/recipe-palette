40901c58c762a2b266e550ce83bbae30971adf66
iepppop <iepppop@gmail.com>
2023-10-09 12:47:38 +0900

    Comment: 주석 제거

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: import.meta.env.VITE_BASE_URL + import.meta.env.VITE_SOME_KEY, 
+  baseURL: import.meta.env.VITE_BASE_URL + import.meta.env.VITE_SOME_KEY, // Vite 환경 변수에서 기본 URL 가져오기
 });
 
 export default instance;
