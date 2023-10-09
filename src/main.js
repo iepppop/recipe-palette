15957b55bb468310125b89e0ac9cfc5160700173
iepppop <iepppop@gmail.com>
2023-10-09 12:46:23 +0900

    feat: axios 전역 설정

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import './assets/main.scss'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import axios from '@/plugins/axios'
 
 import App from './App.vue'
 import router from './router'
@@ -10,5 +11,5 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-
+app.provide('$axios', axios);
 app.mount('#app')
