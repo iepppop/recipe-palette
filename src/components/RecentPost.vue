bd85ef7fd9b48a4714a7e4fae43a8718c15dcc98
iepppop <iepppop@gmail.com>
2023-10-09 17:05:21 +0900

    design: 최근 게시물 css 추가

--- src/components/RecentPost.vue
+++ src/components/RecentPost.vue
@@ -11,7 +11,16 @@
                     <img :src="item.ATT_FILE_NO_MK" :alt="item.RCP_NM"/>
                 </div>
                 <div class="explain">
-
+                    <div class="text">
+                    <div class="tag" :class="item.RCP_PAT2 === '반찬' && 'side-dish'">{{ item.RCP_PAT2 }}</div>
+                    <h2>{{ item.RCP_NM }}</h2>
+                    <p>{{ item.RCP_NA_TIP }}</p>
+                </div>
+                    <div class="sub">
+                       칼로리 <span>{{  item.INFO_ENG }}kcal </span>
+                       <div class="dot"></div>
+                        {{  item.RCP_WAY2 }}
+                    </div>
                 </div>
             </li>
         </ul>
@@ -68,11 +77,104 @@ onMounted(()=>{
             width: 100%;
             display: grid;
             grid-template-columns: repeat(5, 1fr);
-            gap:10px;
+            gap:20px;
 
             li{
                 border:1px solid #f3f3f3;
                 border-radius: 10px;
+                width:100%;
+                overflow: hidden;
+                cursor: pointer;
+
+                &:hover{
+                    box-shadow: 5px 5px 20px #eee;
+                    transition: 0.3s ease-in-out;
+
+                    .image{
+                        img{
+                            scale:1.1;
+                            transition: 0.3s ease-in-out;
+                        }
+                    }
+                }
+
+                .image{
+                    height:60%;
+                    overflow: hidden;
+                    height: 175px;
+                    display: flex;
+                    align-items: center
+                }
+                
+                .explain{
+                    width:100%;
+
+                    .text{
+                        padding:20px;
+                    }
+
+                    .sub{
+                        font-size:13px;
+                        font-weight: 600;
+                        display: flex;
+                        color:#777471; 
+                        border-top:1px solid #f3f3f3;
+                        padding:15px 20px;
+                        align-items: center;
+
+                        .dot{
+                            width:3px;
+                            height:3px;
+                            background:#777471;
+                            border-radius: 50%;
+                            opacity:0.2;
+                            margin:0 10px 0 0;
+                        }
+
+                        span{
+                            color:var(--black);
+                            font-weight: 600;
+                            margin:0 10px;
+                        }
+                    }
+
+                    .tag{
+                        font-size:12px;
+                        font-weight: 600;
+                        color:#f89606;
+                        margin:0 0 10px 0;
+                        background:#fef0db;
+                        width:max-content;
+                        padding:4px 7px 3px 7px;
+                        border-radius: 5px;
+                    }
+
+                    .side-dish{
+                        color:var(--green);
+                        background:#e7f7e7;
+                    }
+
+                    h2{
+                        font-weight: 700;
+                        font-size:15px;
+                    }
+
+                    p{
+                        width: 100%;
+                        height: 40px;
+                        display: block;
+                        margin:10px 0 0 0;
+                        font-size:14px;
+                        font-weight: 500;
+                        display: -webkit-box;
+                        -webkit-line-clamp: 2; /* 라인수 */
+                        -webkit-box-orient: vertical;
+                        word-wrap:break-word; 
+                        line-height: 1.2em;
+                        height: 2.4em;
+                        overflow: hidden;
+                    }
+                }
             }
         }
     }
