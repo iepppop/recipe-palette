43431daff30e312302453c8654de2954ebb084cb
iepppop <iepppop@gmail.com>
2023-10-13 18:51:34 +0900

    feat: 데이터 없을 시 표시 메세지 컴포넌트 추가

---
+++ src/components/NoDataMessage.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="no-data">
+        데이터가 없습니다.
+    </div>
+</template>
+<style lang="scss">
+    .no-data{
+        width:1400px;
+        margin:0 auto;
+    }
+</style>
