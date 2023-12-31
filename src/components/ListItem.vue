ae10e7154b399e6e44258f51c9f763380d8547ef
iepppop <iepppop@gmail.com>
2024-01-01 00:32:12 +0900

    style: 이미지 heigt 변경

--- src/components/ListItem.vue
+++ src/components/ListItem.vue
@@ -229,7 +229,7 @@ const store = useCommonStore();
           z-index: 2;
 
           img {
-            width: 100%;
+            height: 100%;
             object-fit: cover;
           }
         }
